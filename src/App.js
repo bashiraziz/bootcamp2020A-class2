a0e041b229300312f5d915dee27f976b64505095
bashiraziz <bashiraziz@yahoo.com>
2020-06-06 15:54:58 -0400

    Second Try

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          This is some new code, one more try
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
